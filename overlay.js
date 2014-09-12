0b7397624533fa5d73dec7918957725441ea7f3c
Keith Williams <kswillia@andrew.cmu.edu>
2014-09-12 01:23:28 -0400

    close on click logic

--- overlay.js
+++ overlay.js
@@ -1,4 +1,4 @@
-// TODO: create modal block without content or options
+//TODO: render content, assuming it is DOM element
 // NOTE: requires underscore and jquery
 
 window.facade = function(content, options) {
@@ -16,17 +16,27 @@ window.facade = function(content, options) {
     bg : "<div id='facade-background'></div>",
     modal: "<div id='facade-modal'></div>"
   }
+
+  // proxy event methods
+  this.modalClick = $.proxy(this.modalClick, this);
+  this.bgClick = $.proxy(this.bgClick, this);
 }
 
 facade.prototype.render = function() {
   // remove existing dom elements
   _.chain(this.dom).values().invoke('remove');
 
+  // create dom elements
   this.dom.bg = $(this.templates.bg);
   this.dom.modal = $(this.templates.modal);
 
+  // register handlers
+  this.dom.bg.on('click', this.bgClick);
+  this.dom.modal.on('click', this.modalClick);
+
   this.setSize(this.options.width, this.options.height);
 
+  // add elements to body
   var body = $('body');
   body.append(this.dom.bg);
   body.append(this.dom.modal);
@@ -41,6 +51,16 @@ facade.prototype.setSize = function(width, height) {
     'margin-left': -width / 2,
     'margin-top': -height / 2
   });
+}
+
+facade.prototype.modalClick = function(e) {
+  if (this.options.innerClose)
+    this.close();
+}
+
+facade.prototype.bgClick = function(e) {
+  if (this.options.outerClose)
+    this.close();
 }
 
 facade.prototype.open = function() {
