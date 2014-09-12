ba06ced627df9624b4e5b689633684627e32148c
Keith Williams <kswillia@andrew.cmu.edu>
2014-09-11 23:54:51 -0400

    resizing, open and close methods

--- overlay.js
+++ overlay.js
@@ -10,10 +10,45 @@ window.facade = function(content, options) {
     loadMsg: ""
   }
 
-  this.options = _.defaults(options, defaultOptions);
-  this.template = "<div class='background'></div><div class='modal'></div>";
+  this.options = _.extend({}, defaultOptions, options);
+  this.dom = {}
+  this.templates = {
+    bg : "<div id='facade-background'></div>",
+    modal: "<div id='facade-modal'></div>"
+  }
 }
 
 facade.prototype.render = function() {
-  $('body').append(this.template);
+  // remove existing dom elements
+  _.chain(this.dom).values().invoke('remove');
+
+  this.dom.bg = $(this.templates.bg);
+  this.dom.modal = $(this.templates.modal);
+
+  this.setSize(this.options.width, this.options.height);
+
+  var body = $('body');
+  body.append(this.dom.bg);
+  body.append(this.dom.modal);
+
+  this.open();
+}
+
+facade.prototype.setSize = function(width, height) {
+  this.dom.modal.css({
+    'width': width.toString() + "px",
+    'height': height.toString() + "px",
+    'margin-left': -width / 2,
+    'margin-top': -height / 2
+  });
+}
+
+facade.prototype.open = function() {
+  this.dom.bg.show();
+  this.dom.modal.show();
+}
+
+facade.prototype.close = function() {
+  this.dom.bg.hide();
+  this.dom.modal.hide();
 }
